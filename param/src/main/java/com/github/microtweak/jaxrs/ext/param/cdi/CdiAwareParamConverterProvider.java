9182d0888239d81ca880c2a7d9a0c097c73a3557
Marcos Koch Salvador <marcosks0@gmail.com>
2020-03-21 16:38:35 -0300

    Added ParamConverterProvider implementation for CDI beans

---
+++ param/src/main/java/com/github/microtweak/jaxrs/ext/param/cdi/CdiAwareParamConverterProvider.java
@@ -0,0 +1,22 @@
+package com.github.microtweak.jaxrs.ext.param.cdi;
+
+import com.github.microtweak.jaxrs.ext.param.AbstractParamConverterProvider;
+import com.github.microtweak.jaxrs.ext.param.RestParamConverter;
+
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class CdiAwareParamConverterProvider extends AbstractParamConverterProvider {
+
+    @Inject
+    private Instance<RestParamConverter> converters;
+
+    @Override
+    protected List<RestParamConverter> getConverters() {
+        return converters.stream().collect(toList());
+    }
+
+}
